2a1e696bb0f61b6a0bdbb31fe129b03417c8b869
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 09:11:22 +0700

    Add onError in AuthFilter in Gateway server 🫸🌀✏️ 📗 :octocat:

--- gateway-server/src/main/java/com/hendisantika/gatewayserver/filter/AuthFilter.java
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/filter/AuthFilter.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -70,5 +71,11 @@ public class AuthFilter implements GatewayFilter {
             this.populateRequestWithHeaders(exchange, token);
         }
         return chain.filter(exchange);
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
     }
 }

122d2c51e88c254d8fb8a406be7f8357fe4c2432
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 09:24:52 +0700

    Add getType in TypeController in Gateway server 🫸🌀✏️ 📗 :octocat:

--- gateway-server/src/main/java/com/hendisantika/gatewayserver/controller/TypeController.java
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/controller/TypeController.java
@@ -1,8 +1,16 @@
 package com.hendisantika.gatewayserver.controller;
 
+import com.hendisantika.gatewayserver.model.Company;
+import com.hendisantika.gatewayserver.model.Student;
+import com.hendisantika.gatewayserver.model.Type;
 import com.hendisantika.gatewayserver.util.AuthUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -26,4 +34,25 @@ public class TypeController {
     private final RestTemplate restTemplate;
 
     private final AuthUtil authUtil;
+
+    @PostMapping
+    public String getType(@RequestBody Type type, ServerHttpRequest inRequest) {
+        log.info("getting type");
+        log.info("types:" + type.getTypes());
+        type.getTypes().forEach(f -> {
+            if (f.equals("Student")) {
+                log.info("calling first microservice - student");
+                HttpEntity<Student> request = new HttpEntity<>(
+                        new Student(1, "Test", "Student"), setAuthHeader(inRequest.getHeaders().get("userName").toString(), inRequest.getHeaders().get("role").toString()));
+                restTemplate.exchange("http://localhost:8080/first", HttpMethod.POST, request, String.class);
+            }
+            if (f.equals("Company")) {
+                log.info("calling second microservice - company");
+                HttpEntity<Company> request = new HttpEntity<>(
+                        new Company(1, "Test", "Company"), setAuthHeader(inRequest.getHeaders().get("userName").toString(), inRequest.getHeaders().get("role").toString()));
+                restTemplate.exchange("http://localhost:8080/second", HttpMethod.POST, request, String.class);
+            }
+        });
+        return "done";
+    }
 }

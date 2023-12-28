500cbb52dd7d0d49ed3e43cf1d1f62cf3736ace5
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 08:35:13 +0700

    Add FirstController in first service 🫸🌀✏️ 📗 :octocat:

---
+++ first-service/src/main/java/com/hendisantika/firstservice/controller/FirstController.java
@@ -0,0 +1,21 @@
+package com.hendisantika.firstservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-logging-filter2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/28/23
+ * Time: 08:34
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/first")
+public class FirstController {
+}

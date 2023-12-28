04ae32e5a874fe5fe8e21ad9cf567bd333e5e36d
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 09:14:08 +0700

    Add PostGlobalFilter in Gateway server 🫸🌀✏️ 📗 :octocat:

---
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/filter/PostGlobalFilter.java
@@ -0,0 +1,17 @@
+package com.hendisantika.gatewayserver.filter;
+
+import org.springframework.web.server.WebFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-logging-filter2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/28/23
+ * Time: 09:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class PostGlobalFilter implements WebFilter {
+
+}

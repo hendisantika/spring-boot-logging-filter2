db0f7dcdbf686c5cd4659ac1624592d61e0604e9
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 09:14:25 +0700

    Add filter in PostGlobalFilter in Gateway server 🫸🌀✏️ 📗 :octocat:

--- gateway-server/src/main/java/com/hendisantika/gatewayserver/filter/PostGlobalFilter.java
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/filter/PostGlobalFilter.java
@@ -1,6 +1,13 @@
 package com.hendisantika.gatewayserver.filter;
 
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,5 +20,13 @@ import org.springframework.web.server.WebFilter;
  * To change this template use File | Settings | File Templates.
  */
 public class PostGlobalFilter implements WebFilter {
-
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String path = exchange.getRequest().getPath().toString();
+        ServerHttpResponse response = exchange.getResponse();
+        ServerHttpRequest request = exchange.getRequest();
+        DataBufferFactory dataBufferFactory = response.bufferFactory();
+        ServerHttpResponseDecorator decoratedResponse = getDecoratedResponse(path, response, request, dataBufferFactory);
+        return chain.filter(exchange.mutate().response(decoratedResponse).build());
+    }
 }

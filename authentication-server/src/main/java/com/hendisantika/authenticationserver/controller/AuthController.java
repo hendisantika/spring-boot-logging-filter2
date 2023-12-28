2ba1536ed1c43bf2599afd6a9a63e9417171aee8
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 08:52:58 +0700

    Add getToken in AuthController in Authentication server 🫸🌀✏️ 📗 :octocat:

--- authentication-server/src/main/java/com/hendisantika/authenticationserver/controller/AuthController.java
+++ authentication-server/src/main/java/com/hendisantika/authenticationserver/controller/AuthController.java
@@ -1,8 +1,15 @@
 package com.hendisantika.authenticationserver.controller;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +27,16 @@ public class AuthController {
 
     @Value("${jwt.secret}")
     private String secret;
+
+    @GetMapping
+    public String getToken() {
+        System.out.println("inside auth login");
+        return Jwts.builder()
+                .claim("id", "ankitha")
+                .claim("role", "admin")
+                .setSubject("Test Token")
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(10, ChronoUnit.MINUTES)))
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
 }

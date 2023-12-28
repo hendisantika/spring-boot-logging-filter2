95aaec30bc2972a13caf619caafdf458bda0ccec
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 08:59:19 +0700

    Add GatewayConfig in Gateway server 🫸🌀✏️ 📗 :octocat:

---
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.hendisantika.gatewayserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.filters.RequestFilter;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-logging-filter2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/28/23
+ * Time: 08:58
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class GatewayConfig {
+
+    private final RequestFilter requestFilter;
+
+    private final AuthFilter authFilter;
+}

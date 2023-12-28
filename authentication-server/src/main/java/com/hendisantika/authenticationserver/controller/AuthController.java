c8ff686897c04669d2824b51d7a315c96b926490
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 08:53:36 +0700

    Add getTokenProvidingUsernameAndPassword in AuthController in Authentication server 🫸🌀✏️ 📗 :octocat:

--- authentication-server/src/main/java/com/hendisantika/authenticationserver/controller/AuthController.java
+++ authentication-server/src/main/java/com/hendisantika/authenticationserver/controller/AuthController.java
@@ -1,11 +1,11 @@
 package com.hendisantika.authenticationserver.controller;
 
+import com.hendisantika.authenticationserver.model.Credential;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -21,6 +21,7 @@ import java.util.Date;
  * Time: 08:52
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
 @RequestMapping("/login")
 public class AuthController {
@@ -30,10 +31,22 @@ public class AuthController {
 
     @GetMapping
     public String getToken() {
-        System.out.println("inside auth login");
+        log.info("inside auth login");
         return Jwts.builder()
                 .claim("id", "ankitha")
                 .claim("role", "admin")
+                .setSubject("Test Token")
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(10, ChronoUnit.MINUTES)))
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    @PostMapping
+    public String getTokenProvidingUsernameAndPassword(@RequestBody Credential credential) {
+        log.info("inside getTokenProvidingUsernameAndPassword");
+        return Jwts.builder()
+                .claim("id", credential.getUserName())
+                .claim("role", credential.getRole())
                 .setSubject("Test Token")
                 .setIssuedAt(Date.from(Instant.now()))
                 .setExpiration(Date.from(Instant.now().plus(10, ChronoUnit.MINUTES)))

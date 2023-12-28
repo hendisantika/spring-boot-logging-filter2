2f2700b8c90ff9366d2965c7489a0326afec399e
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 09:08:36 +0700

    Add getToken in AuthUtil in Gateway server 🫸🌀✏️ 📗 :octocat:

--- gateway-server/src/main/java/com/hendisantika/gatewayserver/util/AuthUtil.java
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/util/AuthUtil.java
@@ -1,6 +1,11 @@
 package com.hendisantika.gatewayserver.util;
 
+import com.hendisantika.gatewayserver.model.Credential;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,4 +24,15 @@ import org.springframework.web.client.RestTemplate;
 public class AuthUtil {
 
     private final RestTemplate restTemplate;
+
+    public String getToken(String userName, String role) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("userName", userName);
+        headers.set("role", role);
+        HttpEntity<Credential> request = new HttpEntity<>(
+                new Credential("naruto", "admin"), headers);
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8088/login", HttpMethod.POST, request, String.class);
+        System.out.println("token:" + response.getBody());
+        return response.getBody();
+    }
 }

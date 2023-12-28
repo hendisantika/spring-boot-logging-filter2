ddbfe244f584b5204106bc1fc0c4373100257491
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 08:59:46 +0700

    Add getRestTemplate in GatewayConfig in Gateway server 🫸🌀✏️ 📗 :octocat:

--- gateway-server/src/main/java/com/hendisantika/gatewayserver/config/GatewayConfig.java
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/config/GatewayConfig.java
@@ -2,7 +2,9 @@ package com.hendisantika.gatewayserver.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.filters.RequestFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +23,10 @@ public class GatewayConfig {
     private final RequestFilter requestFilter;
 
     private final AuthFilter authFilter;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
 }

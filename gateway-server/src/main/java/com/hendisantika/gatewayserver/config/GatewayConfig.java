4b00dc4cf3770530edefee3640ba1a673698fbdb
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 09:00:30 +0700

    Add customRouteLocator in GatewayConfig in Gateway server 🫸🌀✏️ 📗 :octocat:

--- gateway-server/src/main/java/com/hendisantika/gatewayserver/config/GatewayConfig.java
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/config/GatewayConfig.java
@@ -2,6 +2,8 @@ package com.hendisantika.gatewayserver.config;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.filters.RequestFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -33,5 +35,29 @@ public class GatewayConfig {
     @Bean
     public WebFilter responseFilter() {
         return new PostGlobalFilter();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        // adding 2 rotes to first microservice as we need to log request body if method is POST
+        return builder.routes()
+                .route("first-microservice", r -> r.path("/first")
+                        .and().method("POST")
+                        .and().readBody(Student.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
+                        .uri("http://localhost:8081"))
+                .route("first-microservice", r -> r.path("/first")
+                        .and().method("GET").filters(f -> f.filters(authFilter))
+                        .uri("http://localhost:8081"))
+                .route("second-microservice", r -> r.path("/second")
+                        .and().method("POST")
+                        .and().readBody(Company.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
+                        .uri("http://localhost:8082"))
+
+                .route("second-microservice", r -> r.path("/second")
+                        .and().method("GET").filters(f -> f.filters(authFilter))
+                        .uri("http://localhost:8082"))
+                .route("auth-server", r -> r.path("/login")
+                        .uri("http://localhost:8088"))
+                .build();
     }
 }

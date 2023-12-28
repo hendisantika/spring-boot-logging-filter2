ee39f42ca8e6503408bde6837d733d01cf6ffc7f
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 09:07:42 +0700

    Add AuthUtil in Gateway server 🫸🌀✏️ 📗 :octocat:

---
+++ gateway-server/src/main/java/com/hendisantika/gatewayserver/util/AuthUtil.java
@@ -0,0 +1,22 @@
+package com.hendisantika.gatewayserver.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-logging-filter2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/28/23
+ * Time: 09:07
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@RequiredArgsConstructor
+public class AuthUtil {
+
+    private final RestTemplate restTemplate;
+}
